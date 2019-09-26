8467b60656f05a47818f32ae57f854183ee46c5b
Dipti Parashar <diptiparashar25@gmail.com>
2019-09-25 20:40:44 -0500

    #AlarmClockAndroidApp

---
+++ app/src/main/java/com/demo/android/apps/alarmclock/AlarmDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.demo.android.apps.alarmclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmDisplayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_display);
+    }
+}
